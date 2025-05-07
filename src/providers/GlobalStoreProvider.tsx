24bbaac52ee3ae7becad2bd8ac8d6202a22ed04c
Andrew <andewzhubinskiy21@gmail.com>
2025-05-07 13:06:05 +0300

    fix:first try to add persistance

--- src/providers/GlobalStoreProvider.tsx
+++ src/providers/GlobalStoreProvider.tsx
@@ -3,11 +3,17 @@ import { Layout } from "@/shared/types/types";
 import { Provider } from "react-redux";
 import { makeStore, AppStore } from "@/lib/store";
 import { useRef } from "react";
-
+import { PersistGate } from "redux-persist/integration/react";
 export default function GlobalStoreProvider({ children }: Layout) {
   const storeRef = useRef<AppStore>(undefined);
   if (!storeRef.current) {
     storeRef.current = makeStore();
   }
-  return <Provider store={storeRef.current}>{children}</Provider>;
+  return (
+    <Provider store={storeRef.current}>
+      <PersistGate loading={null} persistor={storeRef.current.__persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 }
